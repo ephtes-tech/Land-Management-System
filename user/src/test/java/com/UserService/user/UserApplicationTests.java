3d37bb1f406f99ade797f0d43ddafd4c7686b90c
ephtes-tech <ephtes51@gmail.com>
2025-11-25 04:12:50 +0300

    Initial commit with db configuration

---
+++ user/src/test/java/com/UserService/user/UserApplicationTests.java
@@ -0,0 +1,13 @@
+package com.UserService.user;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
