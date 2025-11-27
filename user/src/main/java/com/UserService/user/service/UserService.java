05603a918023cf533ef0a3f50d043d5bf32ebe75
ephtes-tech <ephtes51@gmail.com>
2025-11-28 00:00:19 +0300

    Add api error dto

--- user/src/main/java/com/UserService/user/service/UserService.java
+++ user/src/main/java/com/UserService/user/service/UserService.java
@@ -4,6 +4,7 @@ import com.UserService.user.dto.RegistrationDTO;
 
 import com.UserService.user.model.User;
 import com.UserService.user.repo.UserRepository;
+import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -12,21 +13,23 @@ import java.time.LocalDateTime;
 @Service
 @RequiredArgsConstructor
 public class UserService {
-    private final UserRepository userRepository;;
+    private final UserRepository userRepository;
+
+    @Transactional
     public User register(RegistrationDTO dto) {
-      /*  if (usernameExist(registrationDto.getUsername())){
+        if (usernameExist(dto.getUsername())){
             throw new RuntimeException("Username already exist");
         }
-        if (emailExist(registrationDto.getEmail())){
+        if (emailExist(dto.getEmail())){
             throw new RuntimeException("Email already exist");
         }
-        if (phoneExist(registrationDto.getPhoneNumber())){
+        if (phoneExist(dto.getPhoneNumber())){
             throw new RuntimeException("Phone number already used");
         }
 
-        if (NationalIdExist(registrationDto.getNationalId())){
+        if (nationalIdExist(dto.getNationalId())){
             throw new RuntimeException("National id already used");
-        }*/
+        }
 
         User user=dtoToEntity(dto);
         return userRepository.save(user);
@@ -54,7 +57,7 @@ public class UserService {
         return userRepository.existsByUsername(username);
     }
 
-    public boolean NationalIdExist(String NationalId){
+    public boolean nationalIdExist(String NationalId){
         return userRepository.existsByNationalId(NationalId);
     }
 
