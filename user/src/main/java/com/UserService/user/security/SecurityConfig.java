efc21b93d1108035a9bc2f75d6fa1ea82ff26884
ephtes-tech <ephtes51@gmail.com>
2025-12-03 10:16:08 +0300

    Add security cofig file

---
+++ user/src/main/java/com/UserService/user/security/SecurityConfig.java
@@ -0,0 +1,23 @@
+package com.UserService.user.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity)throws Exception{
+        httpSecurity.csrf(csrf->csrf.disable())
+                .authorizeHttpRequests(
+                        auth-> auth.
+                                requestMatchers("/api/user/register").permitAll()
+                                .anyRequest().authenticated()
+                ).oauth2ResourceServer(
+                        oauth->oauth.jwt(jwt->{}));
+        return httpSecurity.build();
+    }
+}
