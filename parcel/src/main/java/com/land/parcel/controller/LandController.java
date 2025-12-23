b9e8d6b2c83b003d53635128f260fe4d3d0fe990
ephtes-tech <ephtes51@gmail.com>
2025-12-23 11:25:47 +0300

    Fix controller errors

--- parcel/src/main/java/com/land/parcel/controller/LandController.java
+++ parcel/src/main/java/com/land/parcel/controller/LandController.java
@@ -5,10 +5,11 @@ import com.land.parcel.dto.LandResponseDto;
 import com.land.parcel.service.LandService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/land")
@@ -17,7 +18,34 @@ public class LandController {
     private final LandService landService;
 
     @PostMapping("/registerLand")
-    public LandResponseDto register(@Valid @RequestBody LandRequestDto landRequestDto){
-        return landService.registerLand(landRequestDto);
+    public ResponseEntity<LandResponseDto> register(@Valid @RequestBody LandRequestDto landRequestDto){
+        return ResponseEntity.ok(landService.registerLand(landRequestDto));
     }
+
+    @GetMapping("/getLand/{id}")
+    public ResponseEntity<LandResponseDto> getLand(@PathVariable Long id){
+        return ResponseEntity.ok(landService.getLand(id));
+    }
+
+    @GetMapping("/getAllLands")
+    public ResponseEntity<List<LandResponseDto>> getAllLands(){
+        return ResponseEntity.ok(landService.getAllLands());
+    }
+
+    @PutMapping("/updateLand/{id}")
+    public ResponseEntity<LandResponseDto> updateLand(@RequestBody LandRequestDto landRequestDto,@PathVariable Long id){
+        return ResponseEntity.ok(landService.updateLand(id, landRequestDto));
+    }
+
+    @DeleteMapping("/deleteLand/{id}")
+    public ResponseEntity<Void> deleteLand(@PathVariable Long id){
+        landService.deleteLand(id);
+        return  ResponseEntity.noContent().build();
+    }
+
+    @GetMapping("/search")
+    public ResponseEntity<List<LandResponseDto>> findByLocation(@RequestParam double longitude,@RequestParam double latitude){
+        return ResponseEntity.ok(landService.findLandByLocation(longitude,latitude));
+    }
+
 }
